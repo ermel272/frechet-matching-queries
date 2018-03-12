488feec4e376e6c554a7c69d31dfaeb51153e61a
Chris Ermel <ermel272@gmail.com>
2018-03-12 16:03:22 -0400

    begin implementing tree query algorithm

--- geometry/curve.py
+++ geometry/curve.py
@@ -31,6 +31,28 @@ class PolygonalCurve2D(object):
         median = int(floor(self.size() / 2))
         return PolygonalCurve2D(self.__points[median:]) if self.size() > 2 else self
 
+    def contains(self, edge):
+        p1 = self.__points[0]
+
+        for point in self.__points[1:]:
+            p2 = point
+            if edge.get_point(0) == p1 and edge.get_point(1) == p2:
+                return True
+            p1 = p2
+
+        return False
+
+    def is_in_left_curve(self, edge):
+        return self.left_curve().contains(edge)
+
+    def is_in_right_curve(self, edge):
+        return self.right_curve().contains(edge)
+
+
+class Edge2D(PolygonalCurve2D):
+    def __init__(self, p1, p2):
+        super(Edge2D, self).__init__([p1, p2])
+
 
 class CurveRangeTree2D(Tree):
     def __init__(self, curve, error):
@@ -71,6 +93,15 @@ class CurveRangeTree2D(Tree):
             return
             yield
 
+    def __partition_path(self, x, y, x_edge, y_edge):
+        x_node = self.__find_node(self.root, x_edge)
+        y_node = self.__find_node(self.root, y_edge)
+
+        # Assumes tree has already been decomposed
+        lca = self.lowest_common_ancestor(x_node, y_node)
+
+        # TODO: Walk down separate paths, reporting nodes in partition
+
     def __build_tree(self, curve, parent=None):
         node = self.Node(curve, self.__error / 2, parent)
 
@@ -80,3 +111,13 @@ class CurveRangeTree2D(Tree):
         node.left = self.__build_tree(curve.left_curve, node)
         node.right = self.__build_tree(curve.right_curve, node)
         return node
+
+    def __find_node(self, node, edge):
+        if node.is_leaf():
+            return node
+        elif node.curve.is_in_left_curve(edge):
+            return self.__find_node(node.left, edge)
+        elif node.curve.is_in_right_curve(edge):
+            return self.__find_node(node.right, edge)
+        else:
+            return None
