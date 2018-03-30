c8c4eaf54fd27930b0fb3364cb4d6a0d5ae39147
Chris Ermel <ermel272@gmail.com>
2018-03-30 00:05:44 -0400

    fixes

---
+++ geometry/tests/curve_tree_test.py
@@ -0,0 +1,21 @@
+import unittest
+
+from geometry.curve import PolygonalCurve2D
+from geometry.point import Point2D
+from geometry.tree import CurveRangeTree2D
+
+
+class TestCurveRangeTree(unittest.TestCase):
+
+    def setUp(self):
+        self.error = 1.0
+        self.delta = 15.0
+
+    def test_init(self):
+        curve = PolygonalCurve2D([
+            Point2D(-5.0, 1.0),
+            Point2D(-4.0, 4.0),
+            Point2D(-2.0, -1.0)
+        ])
+
+        tree = CurveRangeTree2D(curve, self.error, self.delta)
